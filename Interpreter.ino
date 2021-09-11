4914ffa2df6af710383451906779c132426a3951
Dj-Frixz <67787064+Dj-Frixz@users.noreply.github.com>
2021-09-11 12:07:39 +0200

    commit 2 - main file
    created libraries

--- Interpreter.ino
+++ Interpreter.ino
@@ -1,8 +1,8 @@
-#include <LiquidCrystal.h>
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-const int CURSOR_START = 7;
+#include <MDTape.h>
+
 const int stpos = 1, inpos = 3, nxstpos = 5, outpos = 7, dirpos = 9;
+//state, input, pointing (next), output and direction char position on the string (row)
+
 const String MDT[] = {
   "(0,0,0,0,>)",
   "(0,1,0,1,>)",
@@ -50,25 +50,62 @@ const String MDT[] = {
   "(6,-,6,0,>)",
   "(6,=,4,=,>)"
 };
-int cursor = 0;
-char state = MDT[0].charAt(1);
-const int TAPE_START = 50;
+int MDTlength = sizeof(MDT);
+int stbeg = 0;                        //state beginning
+String input = "10";                  //input string
+char cell = input.charAt(0);          //selected cell value
+char state = MDT[0].charAt(stpos);    //actual state
+MDTape tape(input, state);
 
 void setup() {
-  lcd.begin(16, 2);
-  String input_string = "10";
-  char tape[100];
-  for (int i=0; i<input_string.length(); i++){
-    tape[TAPE_START+i] = input_string.charAt(i);
+}
+
+String search_input(char x){
+  int i = stbeg;
+  while(MDT[i].charAt(stpos)==state && cell!=x){
+    i++;
   }
-  lcd.setCursor(0, 1);
-  lcd.print("state:");
-  lcd.setCursor(7, 1);
-  lcd.print(state);
-  lcd.setCursor(CURSOR_START, 0);
-  lcd.print(input_string);
+  if (MDT[i].charAt(stpos)!=state){
+    return "";
+  }
+  return MDT[i];
+}
+
+int search_state(char x){
+  int i = 0;
+  while(i<MDTlength && MDT[i].charAt(stpos)!=x){
+    i++;
+  }
+  if (i==MDTlength){
+    return 0;
+  }
+  stbeg = i;
+  return 1;
 }
 
 void loop() {
-  
+  delay(100);
+  String row = search_input(cell);
+  if (row!=""){
+    char output = row.charAt(outpos);
+    if (output != cell){
+      tape.chvalue(output);
+    }
+    switch(row.charAt(dirpos)){
+      case '>': cell = tape.right(); break;
+      case '<': cell = tape.left(); break;
+      case '-': break;
+      default: return;
+    }
+    char nextstate = row.charAt(nxstpos);
+    if (row.charAt(stpos)!=nextstate){
+      if (search_state(nextstate)==0){
+        return;
+      }else{
+        tape.change_state(nextstate);
+      }
+    }
+  }else{
+    return;
+  }
 }
