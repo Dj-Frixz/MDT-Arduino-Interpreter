5da41ff5cbfa56605d19645d35450812d9baf279
Dj-Frixz <gmsccntdenmark@gmail.com>
2021-10-13 00:34:57 +0200

    Stupid stuff
    making a temporary mess just to simplify saves and transfers

--- Interpreter.ino
+++ Interpreter.ino
@@ -1,5 +1,4 @@
 #include <MDTape.h>
-#include <MemoryFree.h>
 
 int button = 6;
 char input[] = "10";                  //input string
@@ -7,20 +6,16 @@ MDTape tape;
 
 void setup() {
   pinMode(button, INPUT_PULLUP);
-  Serial.begin(9600);
-  Serial.println(freeMemory());
 }
 
 void loop() {
   while (digitalRead(button)==HIGH){}
   if (digitalRead(button)==LOW){
     tape.begin(input);
-    Serial.println(freeMemory());
   }
   while (digitalRead(button)==LOW){}  //waits for user to 'unpress' the button
   while (digitalRead(button)==HIGH){}
   if (digitalRead(button)==LOW){
     tape.mainloop();
-    Serial.println(freeMemory());
   }
 }
